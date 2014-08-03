8702a2dc43c3a79d620f5cfbc1176b13ec6705f9
Ryota Morita <ryota.morita.3.8@gmail.com>
2014-08-03 16:33:11 +0900

    アップロード版

---
+++ game/Assets/Scripts/Life.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+	
+	public GameObject runchanObj;
+	private Runchan runchan;
+	private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		this.runchan = runchanObj.GetComponent<Runchan>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- game/Assets/Scripts/Wepon.cs
+++ game/Assets/Scripts/Wepon.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 
 public class Wepon : MonoBehaviour {
+	public int direction;
+	private Vector3 vdirection;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.vdirection = this.direction == 1 ? Vector3.right : Vector3.left;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var pos = transform.position;
-		pos += Vector3.left * 0.05f;
+		pos += this.vdirection * 0.05f;
 		transform.position = pos;
 	}
 }
